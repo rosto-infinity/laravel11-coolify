5521854ee02b3ab4c5bc3d37a828e4f89db15db3
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-14 06:19:54 +0100

    title
    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,13 @@
                         @endif
                     </header>
 
-                  
+                    <main class="mt-6">
+                       
+                    </main>
+
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    </footer>
                 </div>
             </div>
         </div>
